6ff8eb317675e868316c50f6baf208fe6fda02a7
Kaitlyn Lamgo <lamg1462@bears.unco.edu>
2023-09-22 16:13:56 -0600

    Redid app

--- Student/Superhero/hero/apps.py
+++ Student/Superhero/hero/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NoteConfig(AppConfig):
+class HeroConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'note'
+    name = 'hero'
