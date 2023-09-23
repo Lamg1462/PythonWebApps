f942ce907280acb32d20aa4fb328a96bee7c5b47
Kaitlyn Lamgo <lamg1462@bears.unco.edu>
2023-09-22 19:55:20 -0600

    Added in some coding

--- Student/Superhero/hero/views.py
+++ Student/Superhero/hero/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+class SuperheroListView(TemplateView):
+    template_name = 'superhero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'object_list': Superhero.objects.all()
+        }
+
+
+class SuperheroDetailView(TemplateView):
+    template_name = 'superhero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'hero': Superhero.objects.get(pk=kwargs['pk'])
+        }
