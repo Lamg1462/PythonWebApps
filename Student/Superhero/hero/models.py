fbc46e6c8b80c790c14a8e53832a5f1abfabc964
Kaitlyn Lamgo <lamg1462@bears.unco.edu>
2023-09-22 19:44:56 -0600

    Edited the code

--- Student/Superhero/hero/models.py
+++ Student/Superhero/hero/models.py
@@ -4,3 +4,6 @@ class Superhero(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     image = models.CharField(max_length=200)
+
+    def _str_(self):
+        return f'{self.pk}. {self.name} - {self.description}'
