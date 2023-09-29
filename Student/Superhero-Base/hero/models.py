6a58322b0cfd2bd5d2a9d259f63da26e7eb422e1
Kaitlyn Lamgo <lamg1462@bears.unco.edu>
2023-09-29 15:48:15 -0600

    Added some code

--- Student/Superhero-Base/hero/models.py
+++ Student/Superhero-Base/hero/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Superhero(models.Model):
+    name = models.CharField(max_length=100)
+    strengths = models.TextField()
+    weaknesses = models.TextField()
+    profile_photo = models.ImageField(upload_to='superhero_photos/')
+    
+    def __str__(self):
+        return self.name
--- Student/Superhero-Base/hero/views.py
+++ Student/Superhero-Base/hero/views.py
@@ -1,9 +1,31 @@
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView, DetailView
 
 from.models import Superhero
 
 class HeroListview(ListView):
     template_name = 'hero/lists.html'
     model = Superhero
+
+
+class HeroDetailView(DetailView):
+    template_name = 'hero/detail.html'
+    model = Superhero
+
+
+class HeroCreateView(CreateView):
+    template_name = "hero/create.html"
+    model = Superhero
+    fields = '__all__'
+
+
+class HeroUpdateView(UpdateView):
+    template_name = "hero/edit.html"
+    model = Superherofields = '_all_'
+
+
+class HeroDeleteView(DeleteView):
+    template_name = "hero/delete.html"
+    model = Superhero
+    success_url = reverse_lazy('hero_list')
