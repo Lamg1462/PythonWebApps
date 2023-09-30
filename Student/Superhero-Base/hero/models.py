740cca82fd1a7bf379b44035bb35257e384332ab
Kaitlyn Lamgo <lamg1462@bears.unco.edu>
2023-09-29 19:19:58 -0600

    Fixed spelling

--- Student/Superhero-Base/hero/models.py
+++ Student/Superhero-Base/hero/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Superhero(models.Model):
-    name = models.CharField(max_length=100)
-    identity = models.CharField(max_length=100)
-    description = models.TextField()
-    strengths = models.CharField(max_lenth=100)
-    weaknesses = models.CharField(max_length=100)
-    profile_photo = models.CharField(max_length=100)
+    name = models.CharField(max_length=200, default ="None")
+    identity = models.CharField(max_length=200, default ="None")
+    description = models.TextField(default="None")
+    strengths = models.CharField(max_length=200, default ="None")
+    weaknesses = models.CharField(max_length=200, default ="None")
+    image = models.CharField(max_length=200, default ="None")
     
     def __str__(self):
         return self.name
