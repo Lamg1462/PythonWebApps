dc5f5cc3672fef5ca61fa453893b4e15dc8030c7
Kaitlyn Lamgo <lamg1462@bears.unco.edu>
2023-10-06 21:16:52 -0600

    Added into the superhero app

--- Student/Superhero-Base/config/urls.py
+++ Student/Superhero-Base/config/urls.py
@@ -1,6 +1,8 @@
 from .views import HeroListView, HeroDetailView, HeroCreateView, HeroUpdateView, HeroDeleteView
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', HeroListView.as_view(), name='hero_list'),
@@ -10,4 +12,8 @@ urlpatterns = [
     path('<int:pk>/delete/', HeroDeleteView.as_view(), name='hero_delete'),
     path('admin/', admin.site.urls),
     path('heroes/', include('heroes.urls')),
+    path('register/', auth_views.RegisterView.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
