eeea6d26c046eeddee086f3a447407bd0663af5f
Kaitlyn Lamgo <lamg1462@bears.unco.edu>
2023-10-13 12:57:26 -0600

    Added even more into project 8

---
+++ Student/Articles/Articles/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class SuperheroAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'superhero_app'
+    verbose_name = 'Superhero App'
